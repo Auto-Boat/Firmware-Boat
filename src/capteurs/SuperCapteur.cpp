63663ea2e71e6979e064a513b7eb0767c0958551
Alutulu <arthur.mata24@gmail.com>
2023-02-02 21:40:29 +0100

    OOP for the first captor

---
+++ src/capteurs/SuperCapteur.cpp
@@ -0,0 +1,67 @@
+#include "SuperCapteur.hpp"
+#include <Adafruit_BME280.h>
+
+SuperCapteur::SuperCapteur()
+{
+  Serial.print(F("Initialisation du BME280, à l'adresse [0x"));
+  Serial.print(adresseI2CduBME280, HEX);
+  Serial.println(F("]"));
+
+  if (!bme.begin(adresseI2CduBME280))
+  {
+    Serial.println(F("--> ÉCHEC…"));
+  }
+  else
+  {
+    Serial.println(F("--> RÉUSSIE !"));
+  }
+  Serial.println();
+}
+
+double SuperCapteur::getTemperature()
+{
+  return bme.readTemperature();
+}
+
+void SuperCapteur::printTemperature()
+{
+  Serial.print(F("Température = "));
+  Serial.print(getTemperature());
+  Serial.println(F(" °C"));
+}
+
+double SuperCapteur::getHumidite()
+{
+  return bme.readHumidity();
+}
+
+void SuperCapteur::printHumidite()
+{
+  Serial.print(F("Humidité = "));
+  Serial.print(getHumidite());
+  Serial.println(F(" %"));
+}
+
+double SuperCapteur::getPression()
+{
+  return bme.readPressure();
+}
+
+void SuperCapteur::printPression()
+{
+  Serial.print(F("Pression atmosphérique = "));
+  Serial.print(getPression() / 100.0F);
+  Serial.println(F(" hPa"));
+}
+
+double SuperCapteur::getAltitude()
+{
+  return bme.readAltitude(pressionAuNiveauDeLaMerEnHpa);
+}
+
+void SuperCapteur::printAltitude()
+{
+  Serial.print(F("Altitude estimée = "));
+  Serial.print(getAltitude());
+  Serial.println(F(" m"));
+}
---
+++ src/capteurs/SuperCapteur.hpp
@@ -0,0 +1,27 @@
+#ifndef DEF_SUPER_CAPTEUR
+#define DEF_SUPER_CAPTEUR
+
+#define adresseI2CduBME280 0x76
+#define pressionAuNiveauDeLaMerEnHpa 1024.90
+
+class SuperCapteur
+{
+public:
+  Adafruit_BME280 bme;
+
+  SuperCapteur();
+
+  double getTemperature();
+  void printTemperature();
+
+  double getHumidite();
+  void printHumidite();
+
+  double getPression();
+  void printPression();
+
+  double getAltitude();
+  void printAltitude();
+};
+
+#endif
